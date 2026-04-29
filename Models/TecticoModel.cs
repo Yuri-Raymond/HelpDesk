5d74a358dd8ae28a88300b62378ff93cd9f19bd5
Yuri-Raymond <yuriraymondmessmore@gmail.com>
2026-04-29 12:59:02 -0300

    fazendo a estrutura do sistema

--- Models/TecticoModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace HelpDesk.Models;
-
-public class TecnicoModel : UsuarioModel
-{
-    public string Especialidade { get; set; }
-
-    public List<ChamadoModel> ChamadosAtendidos { get; set; } = new();
-}
